5fa2828fb4e4d5edd5e00bd9f38bc6820049c18c
Varun Goel <varung2812@gmail.com>
2025-09-23 14:28:12 +1000

    Created User Signup, Signin Routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,21 @@
 import { Hono } from 'hono'
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import userRoutes from "./Routes/user"
+import blogRoutes from "./Routes/blog"
 
-const app = new Hono()
+const app = new Hono().basePath('/api/v1');
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route('/user', userRoutes) // Handle /user
+app.route('/blog', blogRoutes) // Handle /book
+
+
+// const prisma = new PrismaClient({
+//     datasourceUrl: env.DATABASE_URL,
+// }).$extends(withAccelerate())
+
+// app.get('/', (c) => {
+//   return c.text('Hello Hono!')
+// })
 
 export default app
