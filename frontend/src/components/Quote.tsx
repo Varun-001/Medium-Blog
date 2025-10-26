9b260cca22f39b95eef8175d9aac4ee4f5ba61b4
Varun Goel <varung2812@gmail.com>
2025-10-26 18:48:30 +1100

    sidecard(quote) component for signup/in page

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,22 @@
+
+export const  Quote = () =>{
+    return (
+        <div className="bg-slate-200 h-screen flex justify-center flex-col">
+            <div className="flex justify-center">
+                <div className="max-w-lg ">
+                    <div className=" text-3xl font-bold"> 
+                        "The Cutomer service I received was exceptional. The support team went above and beyond to address my concern"
+                    </div>
+                    <div className="max-w-md  text-xl font-semibold mt-4">
+                        Varun Goel
+                    </div>
+                    <div className="max-w-md  text-sm font-semibold text-gray-400">
+                        CEO | XYZ Group
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
