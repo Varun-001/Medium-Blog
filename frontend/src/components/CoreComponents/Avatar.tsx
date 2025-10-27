badeba6e135e2ed56dc1d7e61225a2eb61e792ba
Varun Goel <varung2812@gmail.com>
2025-10-27 20:29:40 +1100

    made changes in Avatar Component

--- frontend/src/components/CoreComponents/Avatar.tsx
+++ frontend/src/components/CoreComponents/Avatar.tsx
@@ -1,8 +1,8 @@
 
-export const Avatar = ({ name } : { name : string }) => {
+export const Avatar = ({ name, size= "small" } : { name : string, size?:string }) => {
   return (
-    <div className='relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-gray-600 rounded-full'>
-        <span className='text-xs font-extralight text-gray-600 dark:text-gray-300'>
+    <div className={`relative inline-flex items-center justify-center ${size === "small" ? "w-6 h-6" : "w-10 h-10"} overflow-hidden bg-gray-600 rounded-full`}>
+        <span className={`${size === "small"? "text-xs" : "text-md"} font-extralight text-gray-600 dark:text-gray-300`}>
           {name[0]}
         </span>
         
